9d70fe92ab96e7aa8d65a1aa7994735dd7e3fd60
alyhenr <alyssonhra99@gmail.com>
2024-01-14 11:34:46 -0300

    feat: auth service with jwt authentication

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,12 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
+
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   async create(createUserDto: CreateUserDto) {
-    return this.usersRepository.create(createUserDto);
+    return this.usersRepository.create({
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    });
+  }
+
+  async verifyUser(email: string, password: string) {
+    const foundUser = await this.usersRepository.findOne({
+      email,
+    });
+
+    if (!foundUser) throw new NotFoundException('Email not found');
+    if (!(await bcrypt.compare(password, foundUser.password)))
+      throw new UnauthorizedException('Credentials are not valid');
+
+    return foundUser;
   }
 }
